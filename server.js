5a1303a35576218cb7940426aed5fb575271c4f1
doks-coders <guonnie@gmail.com>
2022-12-31 22:55:39 +0100

    red

--- server.js
+++ server.js
@@ -23,7 +23,7 @@ const server = http.createServer(app)
 
 const io = new Server(server,{
   cors:{
-    origin:'https://foodappapi.onrender.com',
+    origin:'http://localhost:3000',
     methods:['GET',"POST"],
   }
 })
